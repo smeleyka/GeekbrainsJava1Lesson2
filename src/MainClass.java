0f4d394170bfad052572ec3aaa623111da46f15e
smeleyka <smeleyka@gmail.com>
2017-03-30 18:03:01 +0300

    дз урок 2 1-3

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,19 +6,33 @@ import java.util.Arrays;
  */
 public class MainClass {
     public static void main(String[] args) {
-        
-
+        invertArray();
+        fillInArray();
+        multiplyLess6();
     }
 
     public static void invertArray() {
-        int[]array = {1,0,1,1,1,0,0,1,0,1};
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 1) array[i]=0;
-            else array[i]=1;
-            System.out.println(Arrays.toString(array));
+        int[] arr = {1, 0, 1, 1, 1, 0, 0, 1, 0, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) arr[i] = 0;
+            else arr[i] = 1;
         }
-
-
+        System.out.println(Arrays.toString(arr));
     }
 
+    public static void fillInArray() {
+        int[] arr = new int[8];
+        for (int i = 0, j = 0; i < 8; i++, j += 3) {
+            arr[i] = j;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void multiplyLess6() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) arr[i] = arr[i] * 2;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
